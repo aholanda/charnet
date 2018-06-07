24e073d93d2a49e66f9bde8cb02108403366d9c3
Adriano J. Holanda <ajholanda@gmail.com>
2018-06-07 18:01:24 -0300

    fix wrong assigment to NOW variable

--- crypto.mk
+++ crypto.mk
@@ -1,7 +1,8 @@
-NOW := $(date +%Y-%m-%d_%H-%M)
+NOW := $(shell date +%Y-%m-%d_%H-%M)
+
 notes.org.gpg: notes.org
 	-if [ -f $@ ]; then rm -i $@; fi
 	-gpg -c $< \
 	&& cp -v  $< /tmp/$(NOW)-$< \
-	&& git commit -m "notes up to $(NOW)" $@ \
+	&& git commit -m "notes up to $$NOW" $@ \
 	&& git push
