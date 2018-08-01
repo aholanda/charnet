3bf8f3ed49ee4e76b4ae9bf958302ed0493983a3
Adriano J. Holanda <ajholanda@gmail.com>
2018-08-01 11:54:41 -0300

    plot centralities in decreasing order

--- doing.py
+++ doing.py
@@ -7,71 +7,104 @@ import math
 import networkx as nx
 from scipy.stats import *
 
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 # LOCAL
 from books import *
 
-doing = 'assortative'
-def plot(basename, xs, ys, k2knns):
-        (xxs, yys) = ([], [])
-        # calculate the avg of knns
-        for k, knns in sorted(k2knns.items()):
-                m = np.mean(np.array(knns))
-                xxs.append(k)
-                yys.append(m)
-                print(' - ', k, m)
-                
-        fn = '/tmp/' + basename + '-' + doing +'-plt.png'
-        f = open(fn, "w")
-        
+def plot(ys, book_name, ylabel):
+        fn = '/tmp/' + book_name + '-' + ylabel + '-plot.png'
         plt.figure()
-        plt.plot(xxs, yys, label='avg')
-        plt.plot(xs, ys, 'ro', label=basename)
-        plt.xlabel('k')
-        plt.ylabel('$k_{nn}$')
-        plt.grid()
-        plt.title('')
-        plt.legend(fontsize=7, loc='center right')
+        plt.yscale('log')
+        #plt.ylim(0.0, 1.0)
+        plt.plot(ys, 'ro')
+        plt.ylabel(ylabel)
+        plt.title(book_name.title())
         plt.savefig(fn)
-        print('Wrote', fn)
-
+        logger.info('Wrote %s', fn)
+        
 if __name__=='__main__':
-        bs = Books()
-        bs.read()
+        books = Books().get_books()
+        centrs = Graphs.get_centrality_names()
 
-        for b in bs.get_books():
-                k2knns = {} # map degree to average neighbor degree average
-                (xs, ys) = ([], [])
-                G = b.get_graph()
-                H = nx.Graph()
-                fn = '/tmp/' + str(G) +'-'+ doing + '-.csv'
+        for c in centrs:
+                for b in books:
+                        vals = []
+                        fn = '/tmp/' + b.get_name() + '-' + c + '.csv'
+                        f = open(fn)
+                        for ln in f:
+                                (key, val) = ln.rstrip("\n").split(',')
+                                if float(val) > 0.0:
+                                        vals.append(float(val))
 
-                f = open(fn, 'w')
+                        vals = sorted(vals, reverse=True)
+
+                        plot(vals, b.get_name(), c)
+
+# doing = 'assortative'
+# def plot(basename, xs, ys, k2knns):
+#         (xxs, yys) = ([], [])
+#         # calculate the avg of knns
+#         for k, knns in sorted(k2knns.items()):
+#                 m = np.mean(np.array(knns))
+#                 xxs.append(k)
+#                 yys.append(m)
+#                 print(' - ', k, m)
                 
-                for u in G.nodes():
-                        k = G.degree(u)
-                        knn = 0.0 # degree average of neighbors
-                        vs = G.neighbors(u)
-                        for v in vs:
-                                knn += G.degree(v)
+#         fn = '/tmp/' + basename + '-' + doing +'-plt.png'
+#         f = open(fn, "w")
+        
+#         plt.figure()
+#         plt.plot(xxs, yys, label='avg')
+#         plt.plot(xs, ys, 'ro', label=basename)
+#         plt.xlabel('k')
+#         plt.ylabel('$k_{nn}$')
+#         plt.grid()
+#         plt.title('')
+#         plt.legend(fontsize=7, loc='center right')
+#         plt.savefig(fn)
+#         print('Wrote', fn)
 
-                        if len(vs) != 0:
-                                knn /= len(vs)
-                        else:
-                                continue
+# if __name__=='__main__':
+#         bs = Books()
+#         bs.read()
+
+#         for b in bs.get_books():
+#                 k2knns = {} # map degree to average neighbor degree average
+#                 (xs, ys) = ([], [])
+#                 G = b.get_graph()
+#                 H = nx.Graph()
+#                 fn = '/tmp/' + str(G) +'-'+ doing + '-.csv'
+
+#                 f = open(fn, 'w')
+                
+#                 for u in G.nodes():
+#                         k = G.degree(u)
+#                         knn = 0.0 # degree average of neighbors
+#                         vs = G.neighbors(u)
+#                         for v in vs:
+#                                 knn += G.degree(v)
+
+#                         if len(vs) != 0:
+#                                 knn /= len(vs)
+#                         else:
+#                                 continue
                         
-                        xs.append(k)
-                        ys.append(knn)
+#                         xs.append(k)
+#                         ys.append(knn)
 
-                        if k not in k2knns:
-                                k2knns[k] = []
-                        k2knns[k].append(knn)
+#                         if k not in k2knns:
+#                                 k2knns[k] = []
+#                         k2knns[k].append(knn)
 
-                        print(' + ', k, knn)
+#                         print(' + ', k, knn)
                         
-                f.close()
-                plot(str(G), xs, ys, k2knns)
-                print('Wrote ' + fn)
-                #break
+#                 f.close()
+#                 plot(str(G), xs, ys, k2knns)
+#                 print('Wrote ' + fn)
+#                 #break
         
 # def connected_components():
         
