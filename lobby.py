ac9e06b810398b88761b67f59a6166ed68b99024
Adriano J. Holanda <ajholanda@gmail.com>
2018-09-19 16:57:35 -0300

    put logger to write to a file and change signature

--- lobby.py
+++ lobby.py
@@ -1,6 +1,18 @@
+import logging
 from networkx import *
 
-def lobby(G, log_file=None):
+# change INFO to DEBUG to output in "lobby.log" file
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+if logger.getEffectiveLevel() == logging.DEBUG:
+    handler = logging.FileHandler('./lobby.log')
+    formatter = logging.Formatter('%(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+def lobby(G):
     """Lobby or h index
     ================
     
@@ -20,15 +32,13 @@ def lobby(G, log_file=None):
     the Lobby index is 3 because degree $\leq$ neighbor_position. 
     
     """
-    lobbies = []
+    lobbies = {}
 
-    if (log_file!=None):
-        log_file.write('* ' + G.graph['name'] + '\n')
+    logger.debug('* {}'.format(G.graph['name']))
     
-    for u in range(G.number_of_nodes()):
+    for u in G.nodes():
 
-        if (log_file):
-            log_file.write(G.node[u]['name'] + "\tdegree=" + str(G.degree(u)) + '\n')
+        logger.debug('{}\tdegree={}'.format(G.node[u]['name'], str(G.degree(u))))
 
         G.node[u]['Lobby'] = lobby = 1 # initialize lobby
         degs = [] # neighbors' degree
@@ -42,22 +52,18 @@ def lobby(G, log_file=None):
         for deg in degs:
             lobby = idx = idx + 1
 
-            if (log_file!=None):
-                log_file.write("\t" + str(idx) + "\t" + str(deg) + '\n')
+            logger.debug("\t{}\t{}".format(str(idx), str(deg)))
 
             if (deg < idx):
                 lobby = old_idx
                 break
             old_idx = idx
 
-            if (log_file!=None):
-                log_file.write("Lobby=" + str(lobby) + '\n')
+            logger.debug("** Lobby={}".format(str(lobby)))
 
         G.node[u]['Lobby'] = float(lobby) / G.number_of_nodes() # normalize by N vertices
         
-        lobbies.append(G.node[u]['Lobby'])
+        lobbies[u] = G.node[u]['Lobby']
 
-    if log_file:
-        log_file.flush()
     return lobbies
             
