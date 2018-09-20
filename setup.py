4631e3dba5e574b5fd09b4a322259810529219de
Adriano J. Holanda <ajholanda@gmail.com>
2018-09-20 15:50:04 -0300

    add dependencies

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+    
+setup(
+        name='CharNet',
+        version='1.2.dev0',
+        description='manipulate graphs of characters encounter of some books',
+        packages=['charnet',],
+        license='GPL2',
+        long_description=open('LICENSE').read(),
+        install_requires=['networkx', 'numpy', 'pygraphviz'],
+)
