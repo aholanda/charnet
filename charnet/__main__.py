aab47def4797f5182d783a8b9e4f2ccc89a57b52
Adriano J. Holanda <ajholanda@gmail.com>
2018-09-24 16:26:12 -0300

    option to write bicomponents of graph to file

--- charnet/__main__.py
+++ charnet/__main__.py
@@ -27,12 +27,13 @@ def run_all_tasks():
 tasks = [ None, # sys.argv[0] name of the program, no flag associated
           Plot.do, # -c
           Draw.do_graphs, # -g
-          Formatting.write_global_measures, # -b
+          Formatting.write_global_measures, # -m
           Formatting.write_hapax_legomena_table, # -l
           Formatting.write_stat_centralities, # -s
           Formatting.write_vertices_degree, # -d
           Formatting.write_vertices_frequency, # -f
           Formatting.write_edges_weight, # -e
+          Formatting.write_biconnected_comps, # -b
           run_all_tasks] # -a
         
 # headers
@@ -44,17 +45,18 @@ hdrs = ["__main__",
         "\n\t#### TASK 5 - Write statistics of centralities ####",
         "\n\t#### TASK 6 - Write the vertices' degree ####",
         "\n\t#### TASK 7 - Write the characters' frequency ####",
-        "\n\t#### TASK 8 - Write the edges' weight ####", 
+        "\n\t#### TASK 8 - Write the edges' weight ####",
+        "\n\t#### TASK 9 - Write the graph bicomponents ####", 
         "\n\t#### RUNNING ALL TASKS ####"] 
 
 def usage():
-        logger.info(' Usage: ' + sys.argv[0] + ''' [options]
+        print('* Usage: python3 ' + sys.argv[0] + ''' [options]
         OPTIONS
         -p, --plot
         \tPlot the lobby and other centralities comparisons, assortativity mixing and degree distribution with fitting.
         -g, --draw-graph
         \tDraw the graph of characters encounters for visualization generating PNG files.
-        -b, --global
+        -m, --global-measures
         \tWrite global measures in a table in a LaTeX file.
         -l, --legomena
         \tWrite the frequency of hapax legomena, characters that appear only once in a table in a LaTeX file.
@@ -66,12 +68,17 @@ def usage():
         \tWrite the frequency of characters' appearance in a file named \"{dir}/<book_name>-vertex-frequency.csv\".
         -e, --weight
         \tWrite the weight of edges in a file named \"{dir}/<book_name>-edge-weight.csv\".
+        -b, --bicomps
+        \tWrite the graph bicomponents in a file named \"{dir}/<book_name>-bicomponents.txt\".
         -a, --all
         \tExecute all options.
         -o <directory>, --output-dir <directory>
         \tSet the <directory> to write the generated files. Default directory: \"{dir}\"
         -h, --help
         \t Print this help message.
+
+        One of the flags listed above must be selected, with exception of the \"-o\" or 
+        \"--output-dir\" that changes the program behavior and it is optional.
         '''.format(dir=Project.get_outdir())
         )
         exit()
@@ -119,7 +126,7 @@ if __name__ == "__main__":
                                 opts[1] = True
                         elif opt == "-g" or opt == "--draw-graph":
                                 opts[2] = True
-                        elif opt == "-b" or opt == "--global":
+                        elif opt == "-m" or opt == "--global-measures":
                                 opts[3] = True
                         elif opt == "-l" or opt == "--legomena":
                                 opts[4] = True
@@ -131,8 +138,10 @@ if __name__ == "__main__":
                                 opts[7] = True
                         elif opt == "-e" or opt == "--weight":
                                 opts[8] = True
-                        elif opt == "-a" or opt == "--all-tasks":
+                        elif opt == "-b" or opt == "--bicomps":
                                 opts[9] = True
+                        elif opt == "-a" or opt == "--all-tasks":
+                                opts[10] = True
                                 for i in range(1, n-1): # to not repeat tasks
                                         opts[i] = False
                         elif opt == "-h" or opt == "--help": # help make exit
