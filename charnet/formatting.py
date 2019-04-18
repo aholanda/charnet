30aa2b306f31d071468943b9309cfbb588d39062
Adriano J. Holanda <aholanda@usp.br>
2019-04-18 11:29:37 -0300

    put hapax subtables side by side

--- charnet/formatting.py
+++ charnet/formatting.py
@@ -1,7 +1,6 @@
 import os.path
 import numpy as np
 import operator
-import logging
 
 import graph_tool as gt
 import graph_tool.clustering as gt_cluster
@@ -10,18 +9,20 @@ import graph_tool.clustering as gt_cluster
 from plot import *
 from graphs import *
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 class Formatting:
         @staticmethod
-        def write_hapax_legomena_table():
+        def write_hapax_legomena_table(abs=False, dis=False):
                 """"Hapax Legomena The write_hapax_legomena_table() function write the
-                _Hapax_ frequency to be included in the paper using LaTeX
-                syntax for tables.
+                    _Hapax_ frequency to be included in the paper using LaTeX
+                    syntax for tables.
+                Arguments:
+                  abs (Boolean): if true, print out absolute values over the number of characters;
+                  dis (Boolean): if true, print out Hapax DisLegomena that are the number of
+                     characters that appear twice.
                 """
                 n2h = {} # map book name to hapax
                 n2b = {} # map book name to book object
+                tbl = "" # store table content string
                 # Sort books by hapax
                 books = Books.get_books()
                 for book in books:
@@ -32,36 +33,29 @@ class Formatting:
 
                 fn = os.path.join(Project.get_outdir(), 'legomenas.tex')
                 f = open(fn, "w")
-                f.write('''{\\small\\centering\\begin{tabular}{@{}llcc@{}}\\toprule
-                \hfil\\bf  Genre \hfil
-                & \\bf  Book \hfil
-                &  $\\mathbf HL^N=H/N$ & $\\mathbf DL^N=DL/N$ \\\\ \n''')
 
                 for n in Books.get_genre_enums():
-                        ln = '    \colrule\multirow{4}{*}{'+ Books.get_genre_name(n)  + '}' + '\n'
-                        
+                        tbl += '\t\\begin{minipage}{.3\\textwidth}\\centering\n'
+                        tbl += '\t\t\\caption*{' + Books.get_genre_name(n) +'}\\\\ \\smallskip\n'
+                        tbl += '\t\t\\begin{tabular}{@{}p{1.65cm}p{1cm}@{}}\\toprule\n'
+                        tbl += '\t\t\\bf book  & $\\mathbf{HL}$\\\\ \\colrule\n'
+
                         n2h_lst = sorted(n2h.items(), key=operator.itemgetter(1), reverse=True)
                         for bname, hap in n2h_lst:
                                 book = n2b[bname]
                                 enum = book.get_genre()
                                 if enum.value == n:
-                                        nr_dis = book.get_number_dis_legomenas()
+                                        nr_hap = book.get_number_hapax_legomenas()
                                         nr_chars = Graphs.size(book.get_graph())
-                                        ln += '\t&' + book.get_label() + " & "
-                                        ln += '{0:02d}'.format(book.get_number_hapax_legomenas()) + "/"
-                                        ln += '{0:02d}'.format(nr_chars) + " = "
-                                        ln += '{0:.3f}'.format(hap)
-                                        ln += ' & '
-                                        ln += '{0:02d}'.format(nr_dis) + "/"
-                                        ln += '{0:02d}'.format(nr_chars) + " = "
-                                        ln += '{0:.3f}'.format(float(nr_dis)/nr_chars)
-                                        ln +=" \\\\ \n"
-                        
-                        f.write(ln)
+                                        tbl += '\t\t\t' + book.get_label() + ' & '
+                                        tbl += '{0:.2f}'.format(float(nr_hap)/nr_chars)
+                                        tbl += ' \\\\ \n'
+                        tbl += '\t\t\\botrule \\end{tabular}\n'
+                        tbl += '\t\end{minipage}\n'
 
-                f.write("\\botrule \\end{tabular}}\n")
+                f.write(tbl)
                 f.close()
-                logger.info('* Wrote {}'.format(fn))
+                print('* Wrote {}'.format(fn))
 
         @staticmethod
         def write_global_measures():
@@ -74,9 +68,9 @@ class Formatting:
                 [density](https://networkx.github.io/documentation/networkx-1.9/reference/generated/networkx.classes.function.density.html).
                 """
                 logger.info('* Writing global measures...')
-        
+
                 fn = os.path.join(Project.get_outdir(), 'global.tex')
-                
+
                 f = open(fn, "w")
 
                 f.write('''
@@ -100,7 +94,7 @@ class Formatting:
                                         CC,_ = gt_cluster.global_clustering(G)
                                         D = Graphs.density(G)
                                         (deg_avg, deg_stdev) = Graphs.degree_stat(G)
-                
+
                                         # OUTPUT
                                         ln += '\t\t\t&\emph{' + book.get_label() + '} & '
                                         ln += str(len(list(G.vertices()))) + ' & '
@@ -110,7 +104,7 @@ class Formatting:
                                         ln += '{0:.3f}'.format(CC) + ' & '
                                         ln += "\\\\ \n"
                         f.write(ln)
-                                        
+
                 f.write("\t\t\\botrule\\end{tabular}}\n")
 
                 f.close()
@@ -130,7 +124,7 @@ class Formatting:
                                 lab = G.vertex_properties["label"][v]
                                 degs[lab] = v.out_degree()
                                 char_names[lab] = G.vertex_properties["char_name"][v]
-                                
+
                         fn = book.get_name() + suf
                         fn = os.path.join(Project.get_outdir(), fn)
                         f = open(fn, 'w')
@@ -143,7 +137,7 @@ class Formatting:
                                         + sep + str(deg) + '\n')
 
                         f.close()
-                        logger.info('* Wrote {}'.format(fn))                
+                        logger.info('* Wrote {}'.format(fn))
 
         @staticmethod
         def write_vertices_frequency():
@@ -157,7 +151,7 @@ class Formatting:
                         G = book.get_graph()
 
                         fn = book.get_name() + suf
-                        fn = os.path.join(Project.get_outdir(), fn) 
+                        fn = os.path.join(Project.get_outdir(), fn)
                         f = open(fn, 'w')
 
                         for v in G.vertices():
@@ -170,7 +164,7 @@ class Formatting:
                                 f.write(lab + sep + '\"' + char_names[lab] + '\"'+ sep + str(freq) + '\n')
 
                         f.close()
-                        logger.info('* Wrote {}'.format(fn))                
+                        logger.info('* Wrote {}'.format(fn))
 
         @staticmethod
         def write_edges_weight():
@@ -185,20 +179,19 @@ class Formatting:
                         G = book.get_graph()
 
                         fn = book.get_name() + suf
-                        fn = os.path.join(Project.get_outdir(), fn) 
+                        fn = os.path.join(Project.get_outdir(), fn)
                         f = open(fn, 'w')
 
                         for e in G.edges():
                                 u = e.source()
-                                v = e.target()                               
+                                v = e.target()
                                 lab = G.vertex_properties["label"][u] + lnk + G.vertex_properties["label"][v]
                                 ws[lab] = G.edge_properties["weight"][e]
                                 char_names[lab] = '\"' + G.vertex_properties["char_name"][u] + '\"' + lnk \
-                                                  + '\"' + G.vertex_properties["char_name"][v] + '\"' 
+                                                  + '\"' + G.vertex_properties["char_name"][v] + '\"'
 
                         labs=sorted(ws.items(), key=lambda x: x[1], reverse=True)
                         for lab,w in labs:
                                 f.write(lab + sep + char_names[lab] + sep + str(w) + '\n')
                         f.close()
-                        logger.info('* Wrote {}'.format(fn))                
-
+                        logger.info('* Wrote {}'.format(fn))
