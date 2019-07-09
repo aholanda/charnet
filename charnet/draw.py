1a9a96573c8731fcdaacbe6bc07c142b8b0f00f4
Adriano J. Holanda <ajholanda@gmail.com>
2019-07-08 22:16:55 -0300

    fix warnings using pylint

--- charnet/draw.py
+++ charnet/draw.py
@@ -1,41 +1,42 @@
+"""This module contains class to draw graphs."""
+
 import os.path
-import matplotlib.pyplot as plt
 import logging
 
 import graph_tool.draw as gt_draw
 
 # LOCAL
-from books import *
-from plot import *
+from charnet.books import Books, Graphs, Project
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
-class Draw:
-        @staticmethod
-        def do_graphs():
-                '''Graphs for the characters' encounters are drawn for visualization
-                only using matplotlib and NetworkX.'''
-                edge_dict = {}
+class Draw(object):
+    """Draw graphs."""
+    def __init__(self):
+        return
 
-                logger.info('* Drawing graphs...')
-                books = Books.get_books()
-                for book in books:
-                        factor = 10 # increase factor
-                        G = book.get_graph()
-                        color = book.get_vertex_color()
-                        vprop_degrees = Graphs.get_vprop_degrees(G)
+    def __str__(self):
+        return self.__class__.__name__
 
-                        fn = 'g-' + book.get_name() + '.png'
-                        fn = os.path.join(Project.get_outdir(), fn)
-
-                        pos = gt_draw.arf_layout(G, max_iter=0)
-                        gt_draw.graph_draw(G, pos=pos, output=fn,
-                                           vertex_text_color="black",
-                                           vertex_font_size=12,
-                                           vertex_fill_color=color,
-                                           vertex_text=G.vertex_properties["label"],
-                                           vertex_size=vprop_degrees,
-                                           edge_pen_width=G.edge_properties["weight"])
-
-                        logger.info('* Wrote {}'.format(fn))
+    @staticmethod
+    def do_graphs():
+        '''Graphs for the characters' encounters are drawn for visualization
+        only using matplotlib and NetworkX.'''
+        LOGGER.info('* Drawing graphs...')
+        books = Books.get_books()
+        for book in books:
+            graph = book.get_graph()
+            color = book.get_vertex_color()
+            vprop_degrees = Graphs.get_vprop_degrees(graph)
+            file_name = 'g-' + book.get_name() + '.png'
+            file_name = os.path.join(Project.get_outdir(), file_name)
+            pos = gt_draw.arf_layout(graph, max_iter=0)
+            gt_draw.graph_draw(graph, pos=pos, output=file_name,
+                               vertex_text_color="black",
+                               vertex_font_size=12,
+                               vertex_fill_color=color,
+                               vertex_text=graph.vertex_properties["label"],
+                               vertex_size=vprop_degrees,
+                               edge_pen_width=graph.edge_properties["weight"])
+            LOGGER.info('* Wrote %s', file_name)
